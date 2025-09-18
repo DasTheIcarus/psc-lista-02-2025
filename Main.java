c8d5f049678e136795bd4085c14b4c30e44a4378
Matheus Marinho <marinho2matheus@gmail.com>
2025-09-18 19:21:23 -0300

    item 1 - lista 2

---
+++ Main.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner scanner = new Scanner(System.in);
+	    int numero;
+		System.out.println("Digite o número inteiro: ");
+		numero = scanner.nextInt();
+	    System.out.println("O número informado foi " + numero);
+		
+	}
+}
